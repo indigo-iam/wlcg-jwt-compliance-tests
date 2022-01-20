3dbce4b4c7154f91e514238c3845bc4cb95c12a8
Federica Agostini <federica.agostini@cnaf.infn.it>
2022-01-20 12:33:02 +0100

    Correct path to joint-report

--- ci/get-ts-summary-results.sh
+++ ci/get-ts-summary-results.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-cat reports/latest/joint-report.html | grep '^window.output\["stats"\]' | sed -e 's/window.output\["stats"\] = //' -e 's/;$//' | jq '.[0][0] | {pass,fail} | join(" ")' | tr -d '"' | awk '{printf "| All Tests %d | Pass %d | Fail %d|\n", $1+$2, $1, $2}'
+cat reports/reports/latest/joint-report.html | grep '^window.output\["stats"\]' | sed -e 's/window.output\["stats"\] = //' -e 's/;$//' | jq '.[0][0] | {pass,fail} | join(" ")' | tr -d '"' | awk '{printf "| All Tests %d | Pass %d | Fail %d|\n", $1+$2, $1, $2}'
