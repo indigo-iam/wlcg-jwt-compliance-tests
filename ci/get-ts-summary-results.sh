2312460aefd0dba5b3ba16a4bb6541dfaee4834e
Federica Agostini <federica.agostini@cnaf.infn.it>
2022-01-20 16:41:23 +0100

    Move email body into script

--- ci/get-ts-summary-results.sh
+++ ci/get-ts-summary-results.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
-set -ex
+set -e
 
-cat reports/reports/latest/joint-report.html | grep '^window.output\["stats"\]' | sed -e 's/window.output\["stats"\] = //' -e 's/;$//' | jq '.[0][0] | {pass,fail} | join(" ")' | tr -d '"' | awk '{printf "| All Tests %d | Pass %d | Fail %d |\n", $1+$2, $1, $2}'
+if [ $# -lt 1 ]; then
+  exit 1
+fi
+
+report_file="$1"
+
+json_out=$(cat "${report_file}" | grep '^window.output\["stats"\]' | sed -e 's/window.output\["stats"\] = //' -e 's/;$//')
+
+passed_tests=$(echo "${json_out}" | jq .[0][0].pass)
+failed_tests=$(echo "${json_out}" | jq .[0][0].fail)
+
+echo
+echo "Test summary results"
+echo
+echo "All Tests $(( ${passed_tests} + ${failed_tests} ))"
+echo "Pass ${passed_tests}"
+echo "Fail ${failed_tests}"
+echo
+echo "Detailed report at ${BUILD_URL}artifact/reports/reports/latest/joint-report.html"
+echo
+echo "Git repository at https://github.com/indigo-iam/wlcg-jwt-compliance-tests"
+echo
